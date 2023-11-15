46b739d5886a10bf5a4b7ce6e963d1445996666e
kasijatomovic2023 <150952739+kasijatomovic2023@users.noreply.github.com>
2023-11-15 19:02:49 +0100

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+usingUnityEngine;
+
+public class HelloWorld:MonoBehaviour
+{
+
+    void Start()
+   {
+       
+   } 
+
+
+void Update()
+    {   
+
+    }  
+}
